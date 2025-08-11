84ea723a3cee3bfe0435dd079d80f775d172ff7a
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-12 03:28:30 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { View } from 'react-native';
+import { Text } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-8xl font-bold text-blue-500">
+          Welcome to Banti!
+        </Text>
+      </View>
     </>
   );
 }
