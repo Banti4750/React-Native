5443b9cec1dd3e9fd07f077de4d36318792099ac
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-12 04:16:24 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/home.tsx
@@ -0,0 +1,17 @@
+
+import "../global.css"
+import { Linking, Text, View } from "react-native";
+
+function Home() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to home!
+            </Text>
+
+        </View>
+    );
+}
+
+export default Home;
+// const styles = StyleSheet.create({})
