58554e4a0f42d4ff5ab3cda001c255bc7158acab
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-13 07:04:01 +0530

    routeing

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,19 @@
-import { ImageBackground, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 import "../global.css";
-import image from './../assets/images/bg.jpg';
 export default function App() {
     return (
 
         <View className="flex-1 items-center justify-center">
-            <ImageBackground className=" bg-cover w-full h-full" source={image}>
-                <Text className="text-xl font-bold text-blue-500">
-                    Welcome to home!
-                </Text>
-            </ImageBackground>
+            {/* <ImageBackground className=" bg-cover w-full h-full" source={image}> */}
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to home!
+            </Text>
+            <View className="flex flex-row items-center gap-4 bg-slate-500 p-4 rounded-xl">
+                <Link href="/details" className="text-white">View details</Link>
+                <Link href="/explore" className="text-white">Explore Now</Link>
+            </View>
+            {/* </ImageBackground> */}
 
         </View>
     );
